f8894e42ac05caed163086f9c575708ac2b0c06b
xworkzodc <xworkzodc@gmail.com>
2022-03-10 07:57:29 +0530

    Update BagOperator.java

--- Bag/BagOperator.java
+++ Bag/BagOperator.java
@@ -1,27 +1,20 @@
 class BagOperator{
 void display(Bag bag){
-	if(bag instanceof Bag){
+	
     System.out.println(bag.price);
-	}
-	else{
-		System.out.println("bag is not instance of bag");
-	}
-}
-void display(Bag bag1,String name){
-	if(bag1 instanceof wildCraftBag){
+	
+	if(bag instanceof wildCraftBag){
 		wildCraftBag wild=(wildCraftBag)bag1;
 		    System.out.println(wild.warrantyPeriod);
 		    System.out.println(wild.color);
 
 	}
-}
-	void display(Bag bag2,int no){
-		if(bag2 instanceof AmericanTouristerBag){
+	if(bag instanceof AmericanTouristerBag){
 			AmericanTouristerBag casted2=(AmericanTouristerBag)bag2;
 			    System.out.println(casted2.noOfComapartments);
 			    System.out.println(casted2.wallet);
 
-		}
 	}
+}
 
 }
